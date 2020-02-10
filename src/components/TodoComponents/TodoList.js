c6ecb6440abc8a7103e6445bfd75ef59835a62c4
Seth Cox <Sethcox16@yahoo.com>
2020-02-10 18:57:47 -0500

    React.component created in App with state, event handlers.  Todo, TodoForm, TodoList all setup

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from 'react';
+import Item from './Todo';
+
+const ToDoList = props => {
+
+    return (
+        <div className="todo-list">
+            {props.toDo.map( item => (
+                <Item key={item.id} item={item} toggleItem={props.toggleItem} />
+            ))}
+            <button className="clear-btn" onClick={props.removeCompleted}>
+                Clear ToDo
+            </button>
+        </div>
+    )
+};
+
+export default ToDoList;
